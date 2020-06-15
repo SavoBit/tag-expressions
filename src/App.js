9195f254f02ffb271bf0584335130c3bcec9f302
Jinren Ping <30264958+jping0929@users.noreply.github.com>
2020-06-15 11:18:26 -0700

    Add instruction

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ function App() {
           values={['1', '2', '3']}
           ops={['AND', 'OR']}
         />
-
+        <h4>
+          keyboard navigation:
+        </h4>
+        <ul>
+          <li>Use tab and shift+tab to move between different tabs</li>
+          <li>Use up/down arrow to select an option from dropdown menu</li>
+          <li>Hit enter to delete a tag or make a selection </li>
+        </ul>
       </div>
     </div>
   );
